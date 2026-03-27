2e7840a8787189ccfd7ab476e88846058c66b18b
Darren Tucker <daztucker@users.noreply.github.com>
2026-03-27 12:54:04 +1100

    Zero swap and unused filesystem space. (#1)
    This removes a lot of unused cruft from the disk image and allows the
    final compressed image to be and be significantly smaller (1.6G -> 628M,
    in the case of the OpenBSD 7.8 image).
    
    To prevent the *uncompressed* image from being very large, this also
    requires a change to vbox.sh to run the image through "qemu-img convert"
    to discard all of the zero blocks this will create.

--- hooks/finalize.sh
+++ hooks/finalize.sh
@@ -5,9 +5,24 @@
 sed -i 's|$2b$10$qS3/zFLn/6wTQrjNhAddEepvKw.XculyRsXH60FLXjcj5fQeZzIQu||' /etc/master.passwd
 pwd_mkdb -p /etc/master.passwd
 
+# Zero unused disk space on filesystems that have had activity.
+for fs in / /usr /var /tmp; do
+  echo zeroing unused space on $fs
+  dd if=/dev/zero of=$fs/zero bs=1024k >/dev/null 2>&1 || true
+  sync; sync; sync
+  rm -f $fs/zero
+done
+
+# Clear swap space
+swap=$(swapctl -l | awk '/^\/dev/ {print $1}')
+if [ ! -z "$swap" ]; then
+  echo zeroing swap $swap
+  swapctl -d $swap
+  dd if=/dev/zero of=$swap bs=1024k >/dev/null 2>&1
+fi
 
 #enable autologin with root in the console
 #echo "su - root" >>/etc/rc.local
 
 
-
+exit 0
